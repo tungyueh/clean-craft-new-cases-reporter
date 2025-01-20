3c8a21544ff03f4addf87f43e2831d0485272884
tungyuehlin <tungyuehlin@qnap.com>
2025-01-20 16:10:05 +0800

    3. Extract superclass: pull calculation code into a new class

--- README.md
+++ README.md
@@ -6,6 +6,7 @@ This project involves practicing code refactoring based on Chapter 5 of "Clean C
 
 1. Extract fields: totalCases, stateCounts, counties
 2. Extract method: extract out calculate counties and build report
+3. Extract superclass: pull calculation code into a new class
 
 ## Vim tips
 
---
+++ internal/reporter_3_extract_superclass/reporter.go
@@ -0,0 +1,118 @@
+package reporter
+
+import (
+	"fmt"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+type County struct {
+	County         string
+	State          string
+	RollingAverage float64
+}
+
+type NewCasesCalculator struct {
+	totalCases  int
+	stateCounts map[string]int
+	counties    []County
+}
+
+type NewCasesReporter struct {
+	NewCasesCalculator
+}
+
+func (r *NewCasesReporter) MakeReport(countyCsv string) string {
+	lines := strings.Split(countyCsv, "\n")
+	r.calculateCounties(lines)
+
+	report := r.makeHeader()
+	report.WriteString(r.makeCountyDetails())
+	report.WriteString("\n")
+	report.WriteString(r.makeStateTotals())
+	report.WriteString(fmt.Sprintf("Total Cases: %d\n", r.totalCases))
+	return report.String()
+}
+
+func (r *NewCasesReporter) makeStateTotals() string {
+	states := make([]string, 0, len(r.stateCounts))
+	for state := range r.stateCounts {
+		states = append(states, state)
+	}
+	sort.Strings(states)
+	statesTotal := ""
+	for _, state := range states {
+		x := fmt.Sprintf("%s cases: %d\n", state, r.stateCounts[state])
+		statesTotal += x
+	}
+	return statesTotal
+}
+
+func (r *NewCasesReporter) makeCountyDetails() string {
+	details := ""
+	for _, county := range r.counties {
+		x := fmt.Sprintf("%-11s%-10s%.2f\n", county.County, county.State, county.RollingAverage)
+		details += x
+	}
+	return details
+}
+
+func (*NewCasesReporter) makeHeader() *strings.Builder {
+	report := strings.Builder{}
+	report.WriteString("County     State     Avg New Cases\n")
+	report.WriteString("======     =====     =============\n")
+	return &report
+}
+
+func (r *NewCasesCalculator) calculateCounties(lines []string) {
+	r.stateCounts = make(map[string]int)
+	for _, line := range lines {
+		county := r.calcluateCounty(line)
+		r.counties = append(r.counties, county)
+	}
+}
+
+func (r *NewCasesCalculator) calcluateCounty(line string) County {
+	tokens := strings.Split(line, ",")
+	county := County{
+		County: strings.TrimSpace(tokens[0]),
+		State:  strings.TrimSpace(tokens[1]),
+	}
+
+	county.RollingAverage = r.calculateRollingAverage(tokens)
+
+	cases := r.calculateSumOfCases(tokens)
+	r.totalCases += cases
+	r.incrementStateCount(county.State, cases)
+	return county
+}
+
+func (r *NewCasesCalculator) incrementStateCount(state string, cases int) {
+	r.stateCounts[state] = r.stateCounts[state] + cases
+}
+
+func (*NewCasesCalculator) calculateSumOfCases(tokens []string) int {
+	cases := 0
+	for i := 2; i < len(tokens); i++ {
+		val, _ := strconv.Atoi(strings.TrimSpace(tokens[i]))
+		cases += val
+	}
+	return cases
+}
+
+func (*NewCasesCalculator) calculateRollingAverage(tokens []string) float64 {
+	lastDay := len(tokens) - 1
+	firstDay := lastDay - 7 + 1
+	if firstDay < 2 {
+		firstDay = 2
+	}
+	n := float64(lastDay - firstDay + 1)
+	sum := 0
+	for day := firstDay; day <= lastDay; day++ {
+		val, _ := strconv.Atoi(strings.TrimSpace(tokens[day]))
+		sum += val
+	}
+	rollingAverage := float64(sum) / n
+	return rollingAverage
+}
